23156f86f9c0af7ea346ac45414be1af126744e7
Shamarni96 <Smcfarlane926@gmail.com>
2021-06-14 21:01:34 +0100

    get multiple data values from api call

--- server/app.js
+++ server/app.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const exphbs = require("express-handlebars");
+
+const coingeckoApi = require("../lib/coingeckoApi");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-	res.sendFile('views/index.html' , { root : __dirname });
+const handlebarsInstance = exphbs.create({
+	extname: ".html",
+	defaultLayout: "main",
+  });
+
+app.engine("html", handlebarsInstance.engine);
+app.set("view engine", "html");
+app.set("views", __dirname + "/views");
+
+app.use(express.static(__dirname + "/../public"));
+console.log(__dirname + "/../public");
+
+app.get('/', async (req, res) => {
+	const tokensArray  = await coingeckoApi.getCurrentPrice();
+ 	res.render("home", { token : tokensArray });
 });
 
+// app.get("/", async (req, res) => {
+// 	// use the jokeApi required above to call a function that will return the result of the API call.
+// 	const { setup, punchline } = await jokeApi.getJoke();
+// 	res.render("home", { setup, punchline });
+//   });
+
 app.listen(port, () => {
-	//eslint-disable-next-line no-console
 	console.log(`Example app listening at http://localhost:${port}`);
 });
