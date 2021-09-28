83b9002c5d183279398c9effe63eb041ad99943f
Shamarni96 <Smcfarlane926@gmail.com>
2021-09-28 12:13:39 +0100

    get crypto data to show on web page

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const exphbs = require("express-handlebars");
 
 const coingeckoApi = require("../lib/coingeckoApi");
@@ -7,9 +7,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const handlebarsInstance = exphbs.create({
-	extname: ".html",
-	defaultLayout: "main",
-  });
+  extname: ".html",
+  defaultLayout: "main",
+});
 
 app.engine("html", handlebarsInstance.engine);
 app.set("view engine", "html");
@@ -18,17 +18,11 @@ app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/../public"));
 console.log(__dirname + "/../public");
 
-app.get('/', async (req, res) => {
-	const tokensArray  = await coingeckoApi.getCurrentPrice();
- 	res.render("home", { token : tokensArray });
+app.get("/", async (req, res) => {
+  const tokensArray = await coingeckoApi.getCurrentPrice();
+  res.render("home", { token: tokensArray });
 });
 
-// app.get("/", async (req, res) => {
-// 	// use the jokeApi required above to call a function that will return the result of the API call.
-// 	const { setup, punchline } = await jokeApi.getJoke();
-// 	res.render("home", { setup, punchline });
-//   });
-
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
